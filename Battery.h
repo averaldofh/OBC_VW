d989d3a521d9ea3bc3bdef25fe53e2ea1daee09f
Fernando <averaldofh@gmail.com>
2020-03-29 20:26:33 -0400

    Update Battery.h

--- Battery.h
+++ Battery.h
@@ -6,6 +6,6 @@ float getBatVolt()
   { batVolt += analogRead(batSensorPin); }
   batVolt = batVolt/50;
   batVolt = (batVolt * 5) / 4096;
-  batVolt = batVolt / 0.3125;
+  batVolt = batVolt / 0.3125;               //needed because the voltage divider
   return batVolt;
 }
