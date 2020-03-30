d5c3fe62eeea9be2a492300bea2a35b3e0680bc4
Fernando <averaldofh@gmail.com>
2020-03-29 20:26:55 -0400

    Update Fuel.h

--- Fuel.h
+++ Fuel.h
@@ -7,14 +7,5 @@ int getFuelQt(){
   for(int i = 0 ; i < 50 ; i++)
   { fuelqtt += analogRead(FuelSensorPin); }
   fuelqtt = fuelqtt/50;
-  
- if(lastfuel-fuelqtt>100 || fuelqtt-lastfuel>100)
- {
-    lastfuel = fuelqtt;
-    fuelqtt = map(fuelqtt,minCal-10,maxCal+10,0,100);
-    return fuelqtt;
- }
- else {
-   return lastfuel;
-  }
+  return fuelqtt;
 }
