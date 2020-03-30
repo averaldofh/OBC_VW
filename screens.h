7b1ad53a376b396aedc7c7afefb876ed33c860f6
Fernando <averaldofh@gmail.com>
2020-03-29 20:27:51 -0400

    Update screens.h

--- screens.h
+++ screens.h
@@ -1,10 +1,3 @@
-//V0.1
-//
-// MAIN SCREEN
-//  
-//
-//
-//
 #include <U8g2lib.h>  //u8g2 Library
 #include "fusca.h"
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, 16, 15, 4);  //Oled Display
