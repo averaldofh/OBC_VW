0405736c92e0bb553c0e19156ceff66d6e5bd2b4
Fernando <averaldofh@gmail.com>
2020-05-09 16:54:00 -0400

    Update zObsolet.h

--- zObsolet.h
+++ zObsolet.h
@@ -76,8 +76,8 @@ bool drawFuelWarning(int qty)
 //float TempC=0.0;   // variable output
 //float R0=100000;    // value of rct in T0 [ohm]
 //float t0=298.15;   // use T0 in Kelvin [K]
-//float t1=273.15;      // [K] in datasheet 0º C
-//float t2=373.15;      // [K] in datasheet 100° C
+//float t1=273.15;      // [K] in datasheet 0Âº C
+//float t2=373.15;      // [K] in datasheet 100Â° C
 //float RT1=326600;   // [ohms]  resistence in T1
 //float RT2=6792;    // [ohms]   resistence in T2
 //float beta=0.0;    // initial parameters [K]
